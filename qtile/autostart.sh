02cc0e2b7c9d5bd070b1da6abd8cebecdda4e2e7
Justin Gallagher <justingallagher@outlook.com>
2022-07-06 08:11:26 -0400

    copy endeavour OS qtile configuration

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-#feh --bg-scale /path/to/wallpaper
-picom & disown
+feh --bg-scale /usr/share/backgrounds/Community-Wallpapers/eos_wallpapers_community/Endy_vector_satelliet.png
+picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # Power Manager
+#xfce4-power-manager --no-daemon
 xfce4-power-manager & disown
+#exec --no-startup-id sleep 3 && xfce4-power-manager
+
+# Low battery notifier
+~/.config/qtile/scripts/check_battery.sh & disown
+
+# Start welcome
+eos-welcome & disown
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME

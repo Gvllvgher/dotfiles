efdd69f191d6504c5bb4abf348d8d84bd1e9192a
Justin Gallagher <justingallagher@outlook.com>
2022-07-11 17:31:29 -0400

    remove old link.sh and make v2 the primary

--- link.sh
+++ link.sh
@@ -1,33 +1,51 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# Define any files that need to be linked
+linkFiles=( \
+    ".gitconfig" \
+    ".vimrc" \
+    ".zshrc" \
+    ".gtkrc-2.0" \
+    ".config/gtk-3.0/settings.ini" \
+    ".config/neofetch/config.conf" \
+    ".config/xfce4/terminal/terminalrc" \
+    ".config/picom.conf" \
+    ".config/dunst/dunstrc"
+)
+
+# Define any directories that need to be linked
+linkDirs=( \
+    ".config/qtile" \
+    ".config/rofi" \
+)
+
+# Defines the current script directory
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}";     )" &> /dev/null && pwd 2> /dev/null;     )";
 
-# Git
-ln -sf $SCRIPT_DIR/.gitconfig ~/.gitconfig
+# Loop through the linkFiles list
+for file in ${linkFiles[@]}; do
+    # Delete the file if it exists
+    rm ~/$file &> /dev/null
+    
+    # Check if the $file variable contains '/' to
+    # determine if it goes into a subdirectory of ~
+    if [[ "$file" == *"/"* ]]; then
+        # Create the subdirectory if it doesn't exist
+        mkdir -p ~/${file%/*}
+    fi
+    
+    # Link the file
+    ln -sf $SCRIPT_DIR/$file ~/$file
+done
 
-# VIM
-ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
+# Loop through the linkDirs list
+for dir in ${linkDirs[@]}; do
+    # Delete the directory if it exists
+    rm -rf ~/$dir & > /dev/null
+    
+    # Create the parent directory(ies) if it/they don't exist
+    # mkdir -p ~/${dir%/*}
 
-# ZSH
-ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
-
-# GTK Themes
-ln -sf $SCRIPT_DIR/.gtkrc-2.0 ~/.gtkrc-2.0 # GTK 2
-mkdir -p ~/.config/gtk-3.0 # GTK 3
-ln -sf $SCRIPT_DIR/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini # GTK 3
-
-# Qtile
-rm -rf ~/.config/qtile
-ln -sfT $SCRIPT_DIR/.config/qtile ~/.config/qtile
-
-# Neofetch
-rm -rf ~/.config/neofetch
-mkdir -p .config/neofetch
-ln -sf $SCRIPT_DIR/.config/neofetch/config.conf ~/.config/neofetch/config.conf
-
-# XFCE Terminal
-mkdir -p ~/.config/xfce4/terminal
-ln -sf $SCRIPT_DIR/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
-
-# Picom
-ln -sf $SCRIPT_DIR/.config/picom.conf ~/.config/picom.conf
+    # Link the directory
+    ln -sfT $SCRIPT_DIR/$dir ~/$dir
+done

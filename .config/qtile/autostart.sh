3f95d3533bd1dd473780462b8147249a19e1321a
Justin Gallagher <justingallagher@outlook.com>
2022-07-11 19:21:09 -0400

    removing low battery scripts

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -15,10 +15,4 @@ xfce4-power-manager & disown
 xfconf-query -c xsettings -p /Net/IconThemeName -s 'Papirus-Dark'
 xfconf-query -c xsettings -p /Net/ThemeName -s 'Arc-Dark'
 
-# Low battery notifier
-#~/.config/qtile/scripts/check_battery.sh & disown
-
-# Start welcome
-#eos-welcome & disown
-
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME

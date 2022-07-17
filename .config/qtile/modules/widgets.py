7ca800ad107788c51032b251b8cd06b0851f509e
Justin Gallagher <justingallagher@outlook.com>
2022-07-16 21:45:26 -0400

    volume

--- .config/qtile/modules/widgets.py
+++ .config/qtile/modules/widgets.py
@@ -23,33 +23,28 @@ class MyVolume(widget.Volume):
     def _configure(self, qtile, bar):
         widget.Volume._configure(self, qtile, bar)
         self.volume = self.get_volume()
+        
         if self.volume <= 0:
-            self.text = ''
-        elif self.volume <= 15:
             self.text = ''
-        elif self.volume < 50:
-            self.text = ''
         else:
-            self.text = ''
-        # drawing here crashes Wayland
+            self.text = ' ' + str(self.volume) + "%"
+       
+       # drawing here crashes Wayland
 
     def _update_drawer(self, wob=False):
         if self.volume <= 0:
-            self.text = ''
-        elif self.volume <= 15:
             self.text = ''
-        elif self.volume < 50:
-            self.text = ''
         else:
-            self.text = ''
+            self.text = ' ' + str(self.volume) + "%"
+        
         self.draw()
-
+        
         if wob:
             with open(self.wob, 'a') as f:
                 f.write(str(self.volume) + "\n")
 
 volume = MyVolume(
-    fontsize=18,
+    fontsize=15,
     font='JetBrainsMono Nerd Font Regular',
     foreground=colors[4],
     mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn("pavucontrol")}

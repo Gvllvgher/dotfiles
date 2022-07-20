88857ca434d266c91175840a842db6cc9ef72e4f
Justin Gallagher <justingallagher@outlook.com>
2022-07-19 21:48:26 -0400

    configure-archaccount

---
+++ configure-archaccount.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+# Checking if we have sudo password saved
+if sudo -n true 2&>/dev/null; then
+    echo "Sudo password saved, no prompt required."
+else
+    echo "Need sudo privledge, please enter sudo password"
+    sudo touch /etc &> /dev/null
+fi
+
+# Setting User full name
+echo "Please enter your full name:"
+read fullName
+sudo chfn -f "$fullName" $(whoami)
+
+# Setting password
+echo "Please set your password."
+passwd
+
+# Setting root password
+echo "Would you like to set the root account password? [y/N]"
+read setRootPrompt
+
+if [[ "$setRootPrompt" == "y" ]]; then
+    sudo passwd
+    echo "Done setting root password"
+fi
+
+# Setting betterlockscreen as the lock command
+xfconf-query -c xfce4-session -p /general/LockCommand -s "betterlockscreen -l" --create -t string

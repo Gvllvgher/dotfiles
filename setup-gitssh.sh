a1791f3a1131fa8e7a5f73b15d6de98e9879ec90
Justin Gallagher <justingallagher@outlook.com>
2022-11-13 14:25:51 -0500

    didn't change anything

--- setup-gitssh.sh
+++ setup-gitssh.sh
@@ -9,4 +9,4 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 pub=`cat ~/.ssh/id_rsa.pub`
 read -p "Enter github username: " githubuser
 read -s -p "Enter github token for user $githubuser: " githubpass
-curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"$(cat /etc/hostname)-$(date +%m-%d-%Y_%H-%M)\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+echo curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"$(cat /etc/hostname)-$(date +%m-%d-%Y_%H-%M)\",\"key\":\"$pub\"}" https://api.github.com/user/keys

fcfa40fc9c8bce6ae5f5ed6baffa2c92f291efea
Justin Gallagher <justingallagher@outlook.com>
2022-11-13 19:48:26 -0500

    update ssh keystrength

--- setup-gitssh.sh
+++ setup-gitssh.sh
@@ -2,10 +2,11 @@
 
 if [ ! -f ~/.ssh/id_rsa ]; then
   read -p "Enter github email: " email
-  ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa
+  ssh-keygen -t ed25519 -C "$email" -f ~/.ssh/id_rsa
   ssh-add ~/.ssh/id_rsa
 fi
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+ssh-keyscan -t ed25519 github.com >> ~/.ssh/known_hosts
 pub=`cat ~/.ssh/id_rsa.pub`
 read -p "Enter github username: " githubuser
 read -s -p "Enter github token for user $githubuser: " githubpass

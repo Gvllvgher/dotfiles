129a1f00c7def63678aa0991b464515a48f9473d
Justin Gallagher <justingallagher@outlook.com>
2023-03-07 17:27:43 -0500

    treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,10 +3,14 @@ return {
   'prettier/vim-prettier',
   'vim-airline/vim-airline',
   'vim-airline/vim-airline-themes',
-  'sheerun/vim-polyglot',
   'Xuyuanp/nerdtree-git-plugin',
-  { 'catppuccin/nvim', name = 'catppuccin' },
-  { 'neoclide/coc.nvim', branch = 'release' },
-  { 'preservim/nerdtree', branch = 'master' },
+  { 'catppuccin/nvim',
+    name = 'catppuccin' },
+  { 'neoclide/coc.nvim',
+    branch = 'release' },
+  { 'preservim/nerdtree',
+    branch = 'master' },
+  { 'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate' }
 }
 

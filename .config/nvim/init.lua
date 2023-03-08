b81292ac59c89a4549823543b10d599a3c8bc205
Justin Gallagher <justingallagher@outlook.com>
2023-03-08 11:23:32 -0500

    lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- Setup Lazy plugin manager. This will also install plugins
 require("core.lazy")
+-- Import/Execute settings
 require("core.settings")
+-- Import/Execute key mappings
 require("core.keys")

96711c88cc1a52fd15310ca9a436777a08375f6a
Justin Gallagher <justingallagher@outlook.com>
2023-03-09 08:17:50 -0500

    nvimtree config

--- .config/nvim/lua/core/plugins/nvim-tree.lua
+++ .config/nvim/lua/core/plugins/nvim-tree.lua
@@ -4,11 +4,23 @@ return {
     config = function()
       require("nvim-tree").setup({
         sort_by = "case_sensitive",
+        git = {
+          enable = true,
+          ignore = false,
+          show_on_dirs = true,
+          show_on_open_dirs = true,
+          timeout = 400
+        },
+        actions = {
+          open_file = {
+            quit_on_open = true
+          }
+        },
         renderer = {
           group_empty = true,
         },
         filters = {
-          dotfiles = true,
+          dotfiles = false,
         },
       })
     end

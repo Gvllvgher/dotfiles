ce4210765e8f6cf2cc1d019c1764b63e0514846a
Justin Gallagher <justingallagher@outlook.com>
2023-03-15 14:28:25 -0400

    correct picom command

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -10,7 +10,7 @@ hostnamectl status | grep 'Chassis' | grep 'vm' &> /dev/null # Grep looks for VM
 if [[ $? == 0 ]]; then
   # Should prevent screen tearing on most setups if needed
   # if a VM
-  #picom --no-vsync & disown 
+  picom --legacy-backends --no-use-damage --no-vsync & disown 
 else
   # if not a VM
   picom --vsync & disown 
